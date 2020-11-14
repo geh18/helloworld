830adc639b78585cf1dd8a447cf358ca5fce1138
gezim.h90 <gezim.h90@gmail.com>
2020-11-14 15:07:50 +0100

    feat: count

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to our first app"/>
+    <img alt="Vue logo" src="./assets/logo.png"/>
+    <br>
+    I have been clicked: {{ count }} times
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data: () => ({
+    count: 0
+  })
 }
 </script>
 
