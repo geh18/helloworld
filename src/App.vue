ce2945a714cd1f201084ba50c49b218a41ed87f6
gezim.h90 <gezim.h90@gmail.com>
2020-11-14 14:39:05 +0100

    chore: Text changed

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to our vuejs app"/>
+    <HelloWorld msg="Welcome to our first app"/>
   </div>
 </template>
 
