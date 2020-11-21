efe4f6b5e3a15a23add6e3604505ed186a9436fa
Gezim Haziri <geh18@users.noreply.github.com>
2020-11-21 16:33:40 +0100

    feat: canvas

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png"/>
-    <br>
-    <h1>I have been clicked: {{ count }} times</h1>
+    <h1>Ssssssnake</h1>
 
-    <button @click='count = count + 1'>Click to count</button>
+    
+    <canvas id='myCanvas'></canvas>
+
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'App',
   components: {
+  },
+  mounted: function() {
+    let canvas = document.getElementById("myCanvas");
+    let ctx = canvas.getContext("2d");
+
+    let X = 10;
+    
+    ctx.beginPath();
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = "1";
+    ctx.rect(X, 10, 10, 10);
+    ctx.stroke();
+
+    document.addEventListener('keydown', function(event) {
+      if(event.key === 'ArrowRight') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.strokeStyle = "red";
+        ctx.lineWidth = "1";
+        X = X + 5;
+        ctx.rect(X, 10, 10, 10);
+        ctx.stroke();
+      }
+
+      if(event.key === 'ArrowLeft') {
+        console.log('We have pressed LEFT arrow')
+      }
+    });
+
   },
   data: () => ({
     count: 0
@@ -20,6 +50,10 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,5 +61,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+canvas {
+  width: 100%;
+  border: 5px solid rgb(0, 0, 0);
+}
+
+html {
+  overflow: hidden;
 }
 </style>
